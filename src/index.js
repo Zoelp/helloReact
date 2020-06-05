1a550db66d8d9dbb1af2cd603adf5f333f392655
lipei <lipei@taimei-air.com>
2020-06-05 15:14:17 +0800

    init app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Welcome from './Welcome';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Welcome />
   </React.StrictMode>,
   document.getElementById('root')
 );
